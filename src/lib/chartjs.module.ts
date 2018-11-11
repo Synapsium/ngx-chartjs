8684e0a68e30ce67be6c927eba687ebae4deda97
NicolasLaw <nicolas.lawdune@gmail.com>
2018-11-11 22:24:29 +0100

    import Common module

--- src/lib/chartjs.module.ts
+++ src/lib/chartjs.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ChartjsComponent } from './chartjs.component';
 
 @NgModule({
   declarations: [ChartjsComponent],
   imports: [
+    CommonModule
   ],
   exports: [ChartjsComponent]
 })
