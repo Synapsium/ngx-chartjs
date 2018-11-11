e9634db2545e31a7416591ec07d640df2c1e2d59
NicolasLaw <nicolas.lawdune@gmail.com>
2018-11-12 00:57:44 +0100

    implement charjs library

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngx-chartjs';
+
+  charttype: string;
+  chartdata: ChartData;
+  chartoptions: ChartOptions;
+
+  ngOnInit() {
+    this.charttype = 'doughnut';
+    this.chartdata = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          type: "doughnut",
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 120],
+          fill: true,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)',
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)',
+          ],
+          borderWidth: 1,
+        }
+      ],
+    };
+
+    this.chartoptions = {
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart - Animation Progress Bar'
+      }
+    };
+  }
 }
--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -2,8 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChartjsModule, ChartjsConfig, CHARTJS_CONFIG } from '../../../src';
 
-import { ChartjsModule } from 'chartjs';
+const DEFAULT_CHARTJS_CONFIG: ChartjsConfig = {
+  options: {
+    responsive: true,
+    maintainAspectRatio: false
+  }
+};
 
 @NgModule({
   declarations: [
@@ -13,7 +19,12 @@ import { ChartjsModule } from 'chartjs';
     BrowserModule,
     ChartjsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: CHARTJS_CONFIG,
+      useValue: DEFAULT_CHARTJS_CONFIG
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
